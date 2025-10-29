ebd02779cf2c4c5e2c889909bdddd9810126c4f1
terceiroolindina2025 <terceiroolindina2025@gmail.com>
2025-10-29 10:05:11 -0300

    Update testeNuvem.py

--- testeNuvem.py
+++ testeNuvem.py
@@ -1,1 +1,8 @@
-print("Teste")
+from datetime import datetime
+
+def main():
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(f"Script executado automaticamente às {agora} ⏰")
+
+if __name__ == "__main__":
+    main()
